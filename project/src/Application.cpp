b573071a9a2877141ded952a89c7059abc2b14af
AlexBrouillette <alexandre.brouillette101@hotmail.com>
2024-02-12 11:58:38 -0500

    Changement de nom à Team 7 et inclusion du namespace std

--- project/src/Application.cpp
+++ project/src/Application.cpp
@@ -1,8 +1,10 @@
+using namespace std;
+
 #include "Application.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofSetWindowTitle("TP1");
+	ofSetWindowTitle("Team 7");
 
 	renderer.setup();
 }
@@ -45,9 +47,9 @@ void ofApp::mouseDragged(int x, int y, int button){
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
 	if (isImportable && !isRepositioning) { //Si l'importation d'image est active
-		std::list<std::vector<int>>::reverse_iterator imgPos = renderer.imgPosList.rbegin();
-		for (std::list<ofImage>::reverse_iterator iter = renderer.imageList.rbegin(); iter != renderer.imageList.rend(); ++iter) {
-			std::vector temp = *imgPos;
+		list<vector<int>>::reverse_iterator imgPos = renderer.imgPosList.rbegin();
+		for (list<ofImage>::reverse_iterator iter = renderer.imageList.rbegin(); iter != renderer.imageList.rend(); ++iter) {
+			vector temp = *imgPos;
 			if (ofGetMouseX() > temp[0] && ofGetMouseX() < temp[0] + iter->getWidth() && ofGetMouseY() > temp[1] && ofGetMouseY() < temp[1] + iter->getHeight()) {
 				isRepositioning = true;
 				break;
@@ -62,7 +64,7 @@ void ofApp::mousePressed(int x, int y, int button){
 void ofApp::mouseReleased(int x, int y, int button){
 	if (isRepositioning) { //Si une image est en repositionnement
 		isRepositioning = false;
-		std::list<std::vector<int>>::reverse_iterator imgPos = renderer.imgPosList.rbegin();
+		list<vector<int>>::reverse_iterator imgPos = renderer.imgPosList.rbegin();
 		for (int i = imgDistFromMax; i > 0; i--) {
 			++imgPos;
 		}
@@ -94,9 +96,9 @@ void ofApp::gotMessage(ofMessage msg){
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo) {
 	if (isImportable) {
-		std::vector<std::string> imgTypes = { ".png", ".jpg", ".gif" }; //Verifie si le fichier dragged dans la fenetre de l'application est une image
+		vector<string> imgTypes = { ".png", ".jpg", ".gif" }; //Verifie si le fichier dragged dans la fenetre de l'application est une image
 		for (int i = 0; i < dragInfo.files.size(); i++) {
-			if (std::find(imgTypes.begin(), imgTypes.end(), dragInfo.files.at(i).substr(dragInfo.files.at(i).size() - 4, dragInfo.files.at(i).size() - 1)) != imgTypes.end()) {
+			if (find(imgTypes.begin(), imgTypes.end(), dragInfo.files.at(i).substr(dragInfo.files.at(i).size() - 4, dragInfo.files.at(i).size() - 1)) != imgTypes.end()) {
 				renderer.newImage(dragInfo.files.at(i), ofGetMouseX(), ofGetMouseY());
 			}
 		}
